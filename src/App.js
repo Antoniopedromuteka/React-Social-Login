6340669374891200e04305bce16333c905d3852c
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-08-12 00:12:08 +0100

    finalinzdo o header

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Navbar from "./pages/Components/Navbar";
 
  import "./app.css"
+import Home from "./pages/Home";
 
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
     <div className="App">
         <h2>
            <Navbar/>
+           <Home/>
         </h2>
     </div>  
   );
--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
         <span className="logo">Social app</span>
         <ul className="list">
             <li className="listItem">
-                <img src="" alt="" className='avatar'/>
+                <img src="https://scontent.flad2-1.fna.fbcdn.net/v/t39.30808-6/296389097_2266397126844372_5657498016328583755_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFpdtcwqGo8NXZAASR-xUU4FFq4px4EUZcUWrinHgRRl19xmE-3Se4YgINR5vMT9kOvo77gjpMVbcwBDSfqdYH_&_nc_ohc=s2vCBbAetPAAX-qNiNm&tn=UcxUgUzvk4s_C_r7&_nc_ht=scontent.flad2-1.fna&oh=00_AT_TD3rW5bi9pmLP2YrDTztqOQgUus9zX7DW8qcjdL91dw&oe=62FB06B3" alt="" className='avatar'/>
             </li>
             <li className='listItem'>John Doe</li>
             <li className='listItem'>Logout</li>
