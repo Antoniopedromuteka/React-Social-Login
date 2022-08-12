b705a40373e8ad00fd6677a04cf0ba70463af819
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-08-12 02:49:34 +0100

    adciondando o oAuth 2.0 do google

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,33 @@
 
+const cookieSession = require('cookie-session');
 const express = require('express');
+const passport = require('passport');
+const cors = require('cors');
+
+// yarn add express passport cors nodemon cookie-session
 
 const app = express();
 
-console.log("hello world!");
+ app.use(cookieSession(
+    {
+        name: "session",
+        keys: ["lama"],
+        maxAge: 24 * 60 * 60 * 100,
+    }
+ ))
+
+ 
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+app.use(cors({
+
+    origin: "http://localhost:3000",
+    methods: "GET,POST,PUT,DELETE",
+    credentials: true,
+
+}))
 
 app.listen("5000", ()=>{
     console.log("server is running!");
