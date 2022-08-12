ebacf10444ef8e8dd61bab494867e918eb7b7b06
Antoniopedromuteka <antoniomuteka2004@gmail.com>
2022-08-12 02:02:57 +0100

    adicionando as rotas na aplicação

--- src/App.js
+++ src/App.js
@@ -5,15 +5,30 @@ import Home from "./pages/Home";
 import Post from "./pages/Post";
 import Login from "./pages/Login";
 
+import {BrowserRouter, Routes, Route, Link, Navigate} from "react-router-dom"
+
 
 function App() {
+
+  const user = false;
+
   return (
-    <div className="App">
-        <h2>
-           <Navbar/>
-           <Login/>
-        </h2>
-    </div>  
+    <BrowserRouter>
+        <div>
+           <Navbar user={user}/>
+
+           <Routes>
+
+            <Route path="/" element={<Home/>}/>  
+            <Route path="/login" element={user ? <Navigate to="/"/> : <Login/>}/>  
+            <Route path="/post/:id" element={user ? <Post/> : <Navigate to="/login"/>}/>  
+            
+          </Routes> 
+           
+        </div>   
+
+    </BrowserRouter>       
+  
   );
 }
 
--- src/pages/Components/Navbar.jsx
+++ src/pages/Components/Navbar.jsx
@@ -1,18 +1,25 @@
 
 
 import React from 'react'
+import {Link} from "react-router-dom";
 
-const Navbar = () => {
+const Navbar = ({user}) => {
   return (
     <div className='navbar'>
-        <span className="logo">Social app</span>
-        <ul className="list">
-            <li className="listItem">
-                <img src="https://scontent.flad2-1.fna.fbcdn.net/v/t39.30808-6/296389097_2266397126844372_5657498016328583755_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFpdtcwqGo8NXZAASR-xUU4FFq4px4EUZcUWrinHgRRl19xmE-3Se4YgINR5vMT9kOvo77gjpMVbcwBDSfqdYH_&_nc_ohc=s2vCBbAetPAAX-qNiNm&tn=UcxUgUzvk4s_C_r7&_nc_ht=scontent.flad2-1.fna&oh=00_AT_TD3rW5bi9pmLP2YrDTztqOQgUus9zX7DW8qcjdL91dw&oe=62FB06B3" alt="" className='avatar'/>
-            </li>
-            <li className='listItem'>John Doe</li>
-            <li className='listItem'>Logout</li>
-        </ul>
+        <span className="logo"><Link className='link' to="/">Social app</Link></span>
+        {
+            user ? (
+                <ul className="list">
+                <li className="listItem">
+                    <img src="https://scontent.flad2-1.fna.fbcdn.net/v/t39.30808-6/296389097_2266397126844372_5657498016328583755_n.jpg?_nc_cat=104&ccb=1-7&_nc_sid=09cbfe&_nc_eui2=AeFpdtcwqGo8NXZAASR-xUU4FFq4px4EUZcUWrinHgRRl19xmE-3Se4YgINR5vMT9kOvo77gjpMVbcwBDSfqdYH_&_nc_ohc=s2vCBbAetPAAX-qNiNm&tn=UcxUgUzvk4s_C_r7&_nc_ht=scontent.flad2-1.fna&oh=00_AT_TD3rW5bi9pmLP2YrDTztqOQgUus9zX7DW8qcjdL91dw&oe=62FB06B3" alt="" className='avatar'/>
+                </li>
+                <li className='listItem'>John Doe</li>
+                <li className='listItem'>Logout</li>
+            </ul>
+
+            ) : (<Link className='link' to="login/">Login</Link>)
+        }
+   
     </div>
   )
 }
